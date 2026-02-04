37e21ee2fe644137e216e6dee1cc80e6a433125e
markienoodle <mark.balading17@gmail.com>
2026-02-04 08:18:17 +0800

    Added Drizzle files from Drizzle docs

---
+++ db/schema.ts
@@ -0,0 +1,10 @@
+//Sample only, to be replaced with actual Graceline schema. This was only grabbed sa Drizzle docs
+
+import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+});
