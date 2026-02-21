89962bba469b9e104317fc6184dd719da8e5255f
imseff <stephenc.solano@gmail.com>
2026-02-21 12:02:10 +0800

    sidebar trigger for mobile version

--- components/features/dashboard-header.tsx
+++ components/features/dashboard-header.tsx
@@ -1,12 +1,15 @@
 'use client'
 import  {usePathname} from 'next/navigation'
+import { SidebarTrigger } from '../ui/sidebar'
 
 export default function DashboardHeader(){
   const pathname = usePathname()
-  const user = pathname.split('/')[1].charAt(0).toUpperCase() + pathname.slice(2);
+  const user = pathname.split('/')[1].charAt(0).toUpperCase() + pathname.split('/')[1].slice(1)
+
 
   return(
-    <div className="flex items-center pl-5 w-full h-20 border-b-[1] border-b-[#0f172a]/15 font-bold text-2xl">
+    <div className="flex items-center pl-5 w-full h-20 border-b-[1] border-b-[#0f172a]/15 font-bold text-2xl gap-3">
+      <SidebarTrigger className="md:hidden" />
       {user} Dashboard
     </div>
   )
