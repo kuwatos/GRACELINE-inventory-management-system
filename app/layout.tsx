87d7cfe64c83dbb62bc4a266b89c13d6e627ff0f
kuwatos <c.quicho13@gmail.com>
2026-05-12 00:00:32 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css"
-import Sidebar from "@/components/features/sidebar";
+import Sidebar from "@/components/app-sidebar";
 import { Toaster } from "sonner";
 
 const geistSans = Geist({
